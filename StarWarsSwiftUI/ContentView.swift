2bdc8c404df9b39448aeb2dde0afb1571e306399
Yarden Katz <yrdnkz@gmail.com>
2023-04-04 08:59:47 +0200

    Initial Commit

---
+++ StarWarsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsSwiftUI
+//
+//  Created by ירדן כץ on 04/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsSwiftUI/StarWarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSwiftUIApp.swift
+//  StarWarsSwiftUI
+//
+//  Created by ירדן כץ on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
